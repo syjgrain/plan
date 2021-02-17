ee778b9c3b2a2e7d3b10fc3471467fed0d3f2d4c
syjgrain <52773121+syjgrain@users.noreply.github.com>
2021-02-17 17:39:40 +0900

    deco

--- greeting.js
+++ greeting.js
@@ -19,13 +19,13 @@ function handleSubmit(event){
 
 function askForName(){
     form.classList.add(SHOWING_CN);
-    form.addEventListener('submit', handleSubmit)
+    form.addEventListener('submit', handleSubmit);
 }
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `Hello, ${text}`;
 }
 
 function loadName(){
